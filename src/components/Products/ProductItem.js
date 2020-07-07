f78f01e0750bddc30248edd99b920e7570908e20
Mikulew <mikulew@gmail.com>
2020-07-07 14:22:09 +0200

    Install prop-types

--- src/components/Products/ProductItem.js
+++ src/components/Products/ProductItem.js
@@ -1,6 +1,7 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
-const ProductItem = ({ product: { id, title, cover, availability, price } }) => (
+const ProductItem = ({ product: { id, title, cover, availability, price, currency } }) => (
   <div className="card">
     <img src={cover} className="card-img-top" alt={title} />
     <div className="card-body">
@@ -12,5 +13,16 @@ const ProductItem = ({ product: { id, title, cover, availability, price } }) => 
     </div>
   </div>
 );
+
+ProductItem.propTypes = {
+  product: PropTypes.shape({
+    id: PropTypes.number.isRequired,
+    title: PropTypes.string.isRequired,
+    cover: PropTypes.string.isRequired,
+    availability: PropTypes.bool.isRequired,
+    price: PropTypes.number.isRequired,
+    currency: PropTypes.string.isRequired,
+  }).isRequired,
+};
 
 export default ProductItem;
