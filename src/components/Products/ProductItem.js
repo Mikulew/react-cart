a62ebb313c1cb3c34ec174673ef8a73ebbcc04d4
Mikulew <mikulew@gmail.com>
2020-07-07 17:37:50 +0200

    Add disabled class according to availability

--- src/components/Products/ProductItem.js
+++ src/components/Products/ProductItem.js
@@ -3,22 +3,26 @@ import PropTypes from 'prop-types';
 
 import { toFixedNoRounding } from '../../helpers';
 
-const ProductItem = ({ product: { id, title, cover, availability, price, currency } }) => (
-  <div className="card mb-2">
-    <img src={cover} className="card-img-top" alt={title} />
-    <div className="card-body">
-      <h5 className="card-title">{title}</h5>
-      {price && (
-        <p className="card-text card-price alert alert-info m-2 p-1">{`${toFixedNoRounding(
-          price,
-        )} ${currency}`}</p>
-      )}
-      <a href="#" className="btn btn-primary">
-        Add to cart
-      </a>
+const ProductItem = ({ product: { id, title, cover, availability, price, currency } }) => {
+  const checkIsDisabled = () => (availability ? '' : 'disabled');
+
+  return (
+    <div className={`card mb-2 ${checkIsDisabled()}`}>
+      <img src={cover} className="card-img-top" alt={title} />
+      <div className="card-body">
+        <h5 className="card-title">{title}</h5>
+        {price && (
+          <p className="card-text card-price alert alert-info m-2 p-1">{`${toFixedNoRounding(
+            price,
+          )} ${currency}`}</p>
+        )}
+        <a href="#" className={`btn btn-primary card-button ${checkIsDisabled()}`}>
+          Add to cart
+        </a>
+      </div>
     </div>
-  </div>
-);
+  );
+};
 
 ProductItem.propTypes = {
   product: PropTypes.shape({
