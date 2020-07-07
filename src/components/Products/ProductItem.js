35e7535ba97a0a361ab983d5a09127e1b227cdce
Mikulew <mikulew@gmail.com>
2020-07-07 17:19:20 +0200

    Install node-sass and add 'scss' directory and toFixedNoRounding to helpers

--- src/components/Products/ProductItem.js
+++ src/components/Products/ProductItem.js
@@ -1,12 +1,18 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
+import { toFixedNoRounding } from '../../helpers';
+
 const ProductItem = ({ product: { id, title, cover, availability, price, currency } }) => (
-  <div className="card">
+  <div className="card mb-2">
     <img src={cover} className="card-img-top" alt={title} />
     <div className="card-body">
       <h5 className="card-title">{title}</h5>
-      <p className="card-text">{price}</p>
+      {price && (
+        <p className="card-text card-price alert alert-info m-2 p-1">{`${toFixedNoRounding(
+          price,
+        )} ${currency}`}</p>
+      )}
       <a href="#" className="btn btn-primary">
         Add to cart
       </a>
@@ -20,9 +26,13 @@ ProductItem.propTypes = {
     title: PropTypes.string.isRequired,
     cover: PropTypes.string.isRequired,
     availability: PropTypes.bool.isRequired,
-    price: PropTypes.number.isRequired,
+    price: PropTypes.number,
     currency: PropTypes.string.isRequired,
-  }).isRequired,
+  }),
+};
+
+ProductItem.defaultProps = {
+  product: { price: null },
 };
 
 export default ProductItem;
--- src/helpers/index.js
+++ src/helpers/index.js
@@ -1,1 +1,6 @@
 export const convertJSONtoArray = (data) => Object.keys(data).map((key) => data[key]);
+
+export const toFixedNoRounding = (number, decimals = 2) => {
+  const d = 10 ** decimals;
+  return number % 1 === 0 ? Math.trunc(number) : (number * d) / d.toFixed(decimals);
+};
